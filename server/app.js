30ca10094cb0d4d9483468cdbf2fbaabe6a657d4
IsaacYswaro <yisakeswaro@gmail.com>
2024-07-27 22:27:44 -0500

    deploy sql

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 
 // MySQL Connection Pool
 const dbconnection = mysql2.createPool({
-  host: "localhost",
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
